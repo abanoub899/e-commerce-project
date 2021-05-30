96daab43092b00fb7851afeb40ea637a7977bb70
abanoub899 <abanobgayed@outlook.com>
2021-05-30 16:43:09 +0200

    abanoub

--- laravel-8-crud-app/resources/views/layouts/base.blade.php
+++ laravel-8-crud-app/resources/views/layouts/base.blade.php
@@ -42,8 +42,7 @@
 						</div>
 						<div class="topbar-menu right-menu">
 							<ul>
-								<li class="menu-item" ><a title="Register or Login" href="login.html">Login</a></li>
-								<li class="menu-item" ><a title="Register or Login" href="register.html">Register</a></li>
+								
 								<li class="menu-item lang-menu menu-item-has-children parent">
 									<a title="English" href="#"><span class="img label-before"><img src="assets/images/lang-en.png" alt="lang-en"></span>English<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 									<ul class="submenu lang" >
@@ -67,6 +66,31 @@
 										</li>
 									</ul>
 								</li>
+								@if(Route::has('login'))
+								@auth
+								@if (Auth::user()->usertype==='ADMIN')
+								//ADMIN
+								<li class="menu-item menu-item-has-children parent" >
+									<a title="myAccount" href="#">myAccount{{Auth::user()->name}}<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+									<ul class="submenu curency" >
+										<li class="menu-item" >
+											<a title="Dashboard" href="#">Dashboard</a>
+										</li>
+								@else
+								<li class="menu-item menu-item-has-children parent" >
+									<a title="myAccount" href="#">myAccount{{Auth::user()->name}}<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+									<ul class="submenu curency" >
+										<li class="menu-item" >
+											<a title="Dashboard" href="#">Dashboard</a>
+										</li>
+									
+								@endif
+
+								@else
+								<li class="menu-item" ><a title="Register or Login" href="{{route('login')}}">Login</a></li>
+								<li class="menu-item" ><a title="Register or Login" href="{{route('register')}}">Register</a></li>
+								@endauth
+								@endif
 							</ul>
 						</div>
 					</div>
@@ -165,13 +189,13 @@
 									<a href="about-us.html" class="link-term mercado-item-title">About Us</a>
 								</li>
 								<li class="menu-item">
-									<a href="shop.html" class="link-term mercado-item-title">Shop</a>
+									<a href="/shop" class="link-term mercado-item-title">Shop</a>
 								</li>
 								<li class="menu-item">
-									<a href="cart.html" class="link-term mercado-item-title">Cart</a>
+									<a href="/cart" class="link-term mercado-item-title">Cart</a>
 								</li>
 								<li class="menu-item">
-									<a href="checkout.html" class="link-term mercado-item-title">Checkout</a>
+									<a href="/checkout" class="link-term mercado-item-title">Checkout</a>
 								</li>
 								<li class="menu-item">
 									<a href="contact-us.html" class="link-term mercado-item-title">Contact Us</a>
--- laravel-8-crud-app/routes/web.php
+++ laravel-8-crud-app/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home',HomeComponent::class);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',HomeComponent::class);
+Route::get('/shop',ShopComponent::class);
+Route::get('/cart',CartComponent::class);
+Route::get('/checkout',CheckoutComponent::class);
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
